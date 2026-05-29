a2972346822bb3753dd7b17ede00b76cdcc89a73
ZeKaspijs <kaspars1503@gmail.com>
2026-05-29 21:57:06 +0300

    npm run db:setup

---
+++ scripts/setup-db.ts
@@ -0,0 +1,40 @@
+import pool from "../lib/db"
+
+async function setup() {
+    try {
+        await pool.query("CREATE DATABASE IF NOT EXISTS dream4u")
+        console.log("Database created!")
+
+        await pool.query("USE dream4u")
+        console.log("Database in use!")
+
+        await pool.query(`CREATE TABLE IF NOT EXISTS users (
+                              id INT AUTO_INCREMENT PRIMARY KEY,
+                              username VARCHAR(255) NOT NULL,
+                              email VARCHAR(255) NOT NULL UNIQUE,
+                              password VARCHAR(255) NOT NULL
+                         )
+                         `)
+        console.log("Table `users` created!")
+
+        await pool.query(`CREATE TABLE IF NOT EXISTS pending_users (
+                            id INT AUTO_INCREMENT PRIMARY KEY,
+                            username VARCHAR(255) NOT NULL,
+                            email VARCHAR(255) NOT NULL UNIQUE,
+                            password VARCHAR(255) NOT NULL,
+                            token VARCHAR(255) NOT NULL,
+                            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                        )
+                        `)
+        console.log("Table `pending_users` created!")
+
+
+    } catch (e) {
+        console.error("Something went wrong!", e)
+
+    } finally {
+        await pool.end()
+    }
+}
+
+setup()
