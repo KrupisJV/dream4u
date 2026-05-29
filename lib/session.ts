a05e63d32a26e3dd72a782cba66e53813c313ad2
ZeKaspijs <kaspars1503@gmail.com>
2026-05-29 09:50:13 +0300

    Added cookies.

---
+++ lib/session.ts
@@ -0,0 +1,15 @@
+export const sessionOptions = {
+    password: process.env.SESSION_PASSWORD as string,
+    cookieName: "dream4u_session",
+    cookieOptions: {
+        secure: process.env.NODE_ENV === "production"
+    }
+}
+
+export interface SessionData {
+    user?: {
+        id: number
+        username: string
+        email: string
+    }
+}
