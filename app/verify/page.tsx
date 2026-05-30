47feaafb183d43078231f899c228c97a82e8672c
ZeKaspijs <kaspars1503@gmail.com>
2026-05-30 09:42:18 +0300

    Initial Commit. 30.05.2026

---
+++ app/verify/page.tsx
@@ -0,0 +1,8 @@
+export default function Verify() {
+    return (
+        <div>
+            <h1>Email Verified!</h1>
+            <p>Your email has been verified successfully. You can now log in.</p>
+        </div>
+    )
+}
